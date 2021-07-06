11f069706468c6cf6e198720ccf0319b4d817c6f
Lucy Green <Luegreen@gmail.com>
2021-07-06 08:52:01 -0400

    First commit

---
+++ static/js/practice.js
@@ -0,0 +1,12 @@
+var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
+ 
+var sWords = words.filter((word)=>word.startsWith("s"));
+
+console.log(sWords);
+
+
+const countries = ['Norway', 'Sweden',  'Denmark', 'New Zealand'];
+
+const startsWithN = countries.filter((country) => country.startsWith("N"));
+
+console.log(startsWithN);
