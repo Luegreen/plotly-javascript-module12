bb3a31da249007527072d16d89aea94d5d5befbe
Lucy Green <Luegreen@gmail.com>
2021-07-08 12:38:03 -0400

    penultimate work of module

--- practiceEarlyModule/static/js/BellyButton.js
+++ practiceEarlyModule/static/js/BellyButton.js
@@ -1,4 +1,4 @@
-d3.json("samples.json").then(function(data){
+d3.json("data/samples.json").then(function(data){
     wfreq = data.metadata.map(person =>
 person.wfreq).sort((a,b) => b - a);
     filteredWfreq = wfreq.filter(element => element !=
--- practiceEarlyModule/static/js/SpaceX.js
+++ practiceEarlyModule/static/js/SpaceX.js
@@ -1,23 +1,25 @@
 const url = "https://api.spacexdata.com/v2/launchpads";
 
-var LaunchSites = d3.json(url).then(spaceXResults => 
-    console.log("LaunchSites", spaceXResults));
+//var LaunchSites = d3.json(url).then(spaceXResults => 
+//    console.log("LaunchSites", spaceXResults));
 
 
-var latvanLaunchSites= d3.json(url).then(spaceXResults =>
-        console.log("latvanLaunchSites", spaceXResults[0].location.latitude));
+//var latvanLaunchSites= d3.json(url).then(spaceXResults =>
+//        console.log("latvanLaunchSites", spaceXResults[0].location.latitude));
 
+//var BlatvanLaunchSites= d3.json(url).then(spaceXResults =>
+//            console.log("B", spaceXResults.location.latitude));
 
 var MapSites = d3.json(url).then(function(data){
-    latLong = data.spaceXResults.map(place => place.location.latitude)
-    console.log("latLong", latLong)});
+
+    latLong = data.map(place => console.log(place.location.latitude))});
 
 
-var latLongLaunchSites= d3.json(url).then(spaceXResults =>
-    console.log("latLongLaunchSites", spaceXResults.location));
+////var latLongLaunchSites= d3.json(url).then(spaceXResults =>
+//    console.log("latLongLaunchSites", spaceXResults.location));
 
-var siteLatLong = LaunchSites.map(sites => 
-            spaceXResults.location.latitude);
-    sites.location.latitude;
-    console.log("siteLatLong", siteLatLong);
+//var siteLatLong = LaunchSites.map(sites => 
+//            spaceXResults.location.latitude);
+//    sites.location.latitude;
+//    console.log("siteLatLong", siteLatLong);
         
